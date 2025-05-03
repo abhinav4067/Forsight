3fd3c4803be50bf7a294d4a6d92211fa7b382a50
abhinav4067 <abhinavb4067@gmail.com>
2025-05-03 23:05:49 +0530

    added admission page

--- Foresight/Foresight/settings/base.py
+++ Foresight/Foresight/settings/base.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'foresight_app'
+    'foresight_app',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
